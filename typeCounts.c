e6ad09cc1d786d2606377e0bd82c4da9a1b6c51d
Ryan Boehm <ryan.w.boehm@gmail.com>
2017-12-18 08:25:05 -0600

    type count program

---
+++ typeCounts.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* TO COMPILE: clang typeCounts.c -o typeCounts*/
+/* TO RUN: ./typeCounts */
+
+
+int main() {
+
+	printf("Minimum Signed Char %d\n",-(char)((unsigned char) ~0 >> 1) - 1);
+    printf("Maximum Signed Char %d\n",(char) ((unsigned char) ~0 >> 1));
+
+    printf("Minimum Signed Short %d\n",-(short)((unsigned short)~0 >>1) -1);
+    printf("Maximum Signed Short %d\n",(short)((unsigned short)~0 >> 1));
+
+    printf("Minimum Signed Int %d\n",-(int)((unsigned int)~0 >> 1) -1);
+    printf("Maximum Signed Int %d\n",(int)((unsigned int)~0 >> 1));
+
+    printf("Minimum Signed Long %ld\n",-(long)((unsigned long)~0 >>1) -1);
+    printf("Maximum Signed Long %ld\n",(long)((unsigned long)~0 >> 1));
+
+    /* Unsigned Maximum Values */
+    printf("Maximum Unsigned Char %d\n",(unsigned char)~0);
+    printf("Maximum Unsigned Short %d\n",(unsigned short)~0);
+    printf("Maximum Unsigned Int %u\n",(unsigned int)~0);
+    printf("Maximum Unsigned Long %lu\n",(unsigned long)~0);
+
+    return 0;
+}
